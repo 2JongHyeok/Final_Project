9003895b6a205a64cebae6f12256ab0a2dd0d5b9
2JongHyeok <gurpaper@naver.com>
2022-10-04 09:54:58 +0900

    Mario idle and walk animation
    마리오가 가만히 있을때의 애니메이션과 왼쪽, 오른쪽으로 걸어갈때의 애니메이션을 만듬

--- Super_Mario.py
+++ Super_Mario.py
@@ -2,23 +2,32 @@ from pico2d import *
 open_canvas()
 frame = 0
 
-mario_image = load_image('mario.png')
+mario_right_image = load_image('mario_right.png')
+mario_left_image = load_image('mario_left.png')
 
-def mario_idle():
+def mario_idle_right():
     clear_canvas()
-    mario_image.clip_draw(frame*60+2,3,60,90,100,100)
+    mario_right_image.clip_draw(2,512,50,50,100,100)
     update_canvas()
     pass
 
+def mario_walk_right():
+    clear_canvas()
+    mario_right_image.clip_draw(frame*70+2, 512,50,50,100,100) # frame = 1~2 번갈아 가면서 사용
+    update_canvas()
+    pass
 
-mario_idle()
-frame =1
-mario_idle()
+def mario_idle_left():
+    clear_canvas()
+    mario_left_image.clip_draw(860, 512, 50, 50, 100, 100)  # frame = 1~2 번갈아 가면서 사용
+    update_canvas()
+    pass
+
+def mario_walk_left():
+    clear_canvas()
+    mario_left_image.clip_draw(frame * 70 + 2, 512, 50, 50, 100, 100)  # frame = 1~2 번갈아 가면서 사용
+    update_canvas()
+
+mario_walk_left()
 delay(3)
-
-
-
-
-
-
 close_canvas()
