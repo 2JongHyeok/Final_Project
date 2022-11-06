31461430dab38a3b3f6cda533acbe712c547cbd9
2JongHyeok <gurpaper@naver.com>
2022-11-06 15:53:16 +0900

    Update SuperMario.py
    마리오 코드 수정 및, 초기상태 저장

--- SuperMario.py
+++ SuperMario.py
@@ -23,7 +23,8 @@ key_event_table = {
 class IDLE:
     def enter(self, event):
         self.dir = 0
-        self.need_frame
+        self.need_frame = 1
+        self.frame = 0
 
     def exit(self,event):
         pass
@@ -33,7 +34,21 @@ class IDLE:
         pass
 
     def draw(self):
-        pass
+        if self.face_dir == 1:
+            if MARIO.small_mario:
+                MARIO.right_image.clip_draw(2, 512, 50, 50, MARIO.draw_mario_x, MARIO.real_mario_y)
+                MARIO.need_frames = 1
+            else:
+                MARIO.right_image.clip_draw(2, 306, 50, 70, MARIO.draw_mario_x, MARIO.real_mario_y)
+                MARIO.need_frames = 1
+        else:
+            if MARIO.small_mario:
+                MARIO.left_image.clip_draw(860, 512, 50, 50, MARIO.draw_mario_x,
+                                           MARIO.real_mario_y)  # frame = 1~2 번갈아 가면서 사용
+                MARIO.need_frames = 1
+            else:
+                MARIO.left_image.clip_draw(860, 306, 50, 70, MARIO.draw_mario_x, MARIO.real_mario_y)
+                MARIO.need_frames = 1
 
 
 class WALK:
@@ -113,7 +128,6 @@ class SHOOT:
 
 class MARIO:
     def __init__(self):
-        self.x, self.y = 100, 40
         self.frame = 0
         self.dir, self.face_dir = 0, 1
         self.right_image = load_image('mario_right.png')
@@ -123,7 +137,42 @@ class MARIO:
         self.y_velocity = self.jump_height
         self.pre_y = self.real_mario_y
         self.event_que = []
-        
+        self.cur_state = IDLE
+        self.cur_state.enter(self, None)
+        self.small_mario = True
+        self.draw_mario_x = 100
+        self.real_mario_x = 100
+        self.real_mario_y = 40
+
+    def update(self):
+        self.cur_state.do(self)
+        if self.event_que:
+            event = self.event_que.pop()
+            self.cur_state.exit(self, event)
+
+            try:
+                self.cur_state = next_state[self.cur_state][event]
+            except KeyError:
+                print('ERROR', {self.cur_state.__name__}, '  ', event_name[event])
+            self.cur_state.enter(self, event)
+
+    def draw(self):
+        self.cur_state.draw(self)
+        debug_print('PPPP')
+        debug_print(f'Face Dir: {self.face_dir}, Dir: {self.dir}')
+
+    def add_event(self, event):
+        self.event_que.insert(0, event)
+
+    def handle_event(self, event):
+        if (event.type, event.key) in key_event_table:
+            key_event = key_event_table[(event.type, event.key)]
+            self.add_event(key_event)
+
+    # def fire_ball(self):
+    #     print('FIRE BALL')
+    #     ball = Ball(self.x, self.y, self.face_dir*2)
+    #     game_world.add_object(ball, 1)
 
 
 
